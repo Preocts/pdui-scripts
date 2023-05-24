91797ab8ad5cf47b974bd875f5a792a0ac99c49f
Preocts <preocts@preocts.com>
2023-05-23 23:42:06 -0400

    Adjust naming

--- noteform-on-top.user.js
+++ noteform-on-top.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         flipnotebox
+// @name         noteform-on-top
 // @namespace    https://github.com/Preocts
 // @version      0.1
 // @description  Flip the note box on PagerDuty to the top of the list of current notes
@@ -15,20 +15,20 @@ const elementName = 'Note form';
 (function () {
   'use strict';
   if (!urlMatch.test(window.location.href)) {
-    console.log('[flipnotebox] Not on an incident page');
+    console.log('[noteform-on-top] Not on an incident page');
     return;
   }
 
   new MutationObserver((_, observer) => {
     const noteForm = document.getElementsByName(elementName);
     if (!noteForm || noteForm.length === 0) {
-      console.log('[flipnotebox] Note form not found')
+      console.log('[noteform-on-top] Note form not found')
       return;
     }
     const noteWidget = noteForm[0].parentNode;
     // If the widget is not the first child, move it to the top
     if (noteWidget && noteForm[0].previousElementSibling) {
-      console.log('[flipnotebox] Moving note form to top of widget');
+      console.log('[noteform-on-top] Moving note form to top of widget');
       noteWidget.insertBefore(noteForm[0], noteWidget.firstChild);
       observer.disconnect();
     };
